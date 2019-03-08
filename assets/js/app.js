a12bc311b103323a62b0febfcf82e2293ff01daf
Kgor <kevin.gorham@gmail.com>
2019-03-08 11:11:10 -0800

    chanced index.html so the main-content card is inside the col-sm-2 div

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+// Psuedo Code KNA Music Project
+
+// create one js file to deal with:
+    // initial page
+        // add event listener for main page submit button
+        // on.click -> 
+            // capture the text in the input form and feed it to firebase
+            // open our main content page   
+    
+// Main Content Page
+    // populate the page with pregenerated buttons
+        // could TM feed us info on similar artists to populate our buttons with?     
+    // add a button for the search term used on initial page - get the info from firebase
+    // populate #main-content with information about the search term used on the initial page
+        // Title of artist/band
+        // Info on 3-5 next shows
+            // date, venue, time permitting info from a wiki api
+            // link to buy tickets for each show - tmAPI 
+        // If you click the venue name (or create a button for this),
+            // capture the venue address or perhaps just name and feed it to the googleAPI
+            // change the img card/container/div to be a google map of the venue location 
+    // add an img from tm API of the artist to the page 
+        // this img will turn into the map when the venue button is clicked on 
+
+        // listen for search click 
+
+
+
