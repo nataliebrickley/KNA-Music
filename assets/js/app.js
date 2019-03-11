0c1289a9090a06ea69c50f26c671b9a620c17b8a
Kgor <kevin.gorham@gmail.com>
2019-03-11 12:43:57 -0700

    fixed some map issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -63,6 +63,7 @@ $("#submit").on("click", function () {
         venue.attr("data-lng", eventsArray[0]._embedded.venues[0].location.longitude);
         latitude = parseFloat(eventsArray[0]._embedded.venues[0].location.latitude);
         longitude = parseFloat(eventsArray[0]._embedded.venues[0].location.longitude);
+        venueName =  eventsArray[0]._embedded.venues[0].name;
         console.log("latitdue: " + latitude + " Longitude: " + longitude);
         venue.attr("id", "venue");
         $("#venue").append(venue);    
@@ -90,6 +91,7 @@ $("#submit").on("click", function () {
         var info = $("<p>").text(eventsArray[0].info)
         $("#info").append(info)
 
+        initMap();
     })
     
     
--- assets/js/gmaps.js
+++ assets/js/gmaps.js
@@ -15,23 +15,31 @@
 var map;
 var latitude = 32.715;
 var longitude = -117.161;
+var myLatLng = { lat: latitude, lng: longitude};
+var venueName = 'San Diego';
 
 // center maps on venue when ....
 // ... the user clicks on the venue name
 // $(document).on("click", "#venue", function () {
-    console.log("lat: " + latitude + "lng: " + longitude);
-    // Initialize the map.
-    var map;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
+console.log("lat: " + latitude + "lng: " + longitude);
+// Initialize the map.
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: latitude, lng: longitude},
         zoom: 12
+    });
+    var marker = new google.maps.Marker({
+        position: {lat: latitude, lng: longitude},
+        map: map,
+        title: venueName,
       });
-    }
+};
 
-    // var script = $("<script scr='https://maps.googleapis.com/maps/api/js?key=AIzaSyBnIs9SWvCoCykKiXFp_MvafcDjcSrh0DU&callback=initMap' async defer>");
-    // script.appendTo("#gmap-script");
+// var script = $("<script scr='https://maps.googleapis.com/maps/api/js?key=AIzaSyBnIs9SWvCoCykKiXFp_MvafcDjcSrh0DU&callback=initMap' async defer>");
+// script.appendTo("#gmap-script");
 // });
 
 $(document).on("click", "#venue", function () {
-    initMap(); });
+    initMap();
+})
